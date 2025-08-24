2d17ebec5424c5e2f17fcb3e0750798c2aa47ccb
Shiva Krishna <shivakrishna6032@gmail.com>
2025-08-25 00:48:43 +0530

    .

--- backend/src/utils/responses.js
+++ backend/src/utils/responses.js
@@ -1,9 +1,9 @@
-
 export const sendAuthCookie = (res, name, token, days, isProd) => {
   res.cookie(name, token, {
     httpOnly: true,
     secure: isProd,
-    sameSite: isProd ? "None" : "lax",
+    sameSite: "None",
+    domain: ".lead-management-system-psi.vercel.app",
     maxAge: days * 24 * 60 * 60 * 1000,
     path: "/",
   });
